c96ca7d06264cef20af4b0b0519374f4c7b174b5
Nate Boxer <nboxer@usc.edu>
2025-09-23 15:15:49 -0700

    .

---
+++ script.zsh
@@ -0,0 +1,19 @@
+echo "Paste your curl command, then press Ctrl+D: "
+read -d '' curl_cmd
+rest=$(echo "$curl_cmd" | tail -n +2)
+
+new_cmd="curl -o /dev/null -s -w "%{http_code}" 'https://www.instagram.com/graphql/query'"
+final_cmd="$new_cmd
+$rest"
+
+for ((i = 0; i < 5000; i++)); do
+	status_code=$(eval "$final_cmd")
+
+	if [[ "$status_code" != "200" ]]; then
+		echo "Successfully sent $i before getting an error: $status_code"
+		echo "Try copy/pasting the request again and rerunning this command" 
+		break
+	fi
+	echo "$i: success"
+
+done
