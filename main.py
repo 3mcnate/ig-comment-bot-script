4c260aba0f267e9616444568940fa80f06b72f04
Nate Boxer <nboxer@usc.edu>
2025-09-24 14:39:35 -0700

    python script written

--- main.py
+++ main.py
@@ -1,14 +1,23 @@
-import requests
+import time
 import pyperclip
 import re
+import sys
 import subprocess
 from urllib.parse import quote
+import random
 
 class CommentCurlRequest:
 
 	def __init__(self, command: str):
 		self.raw_command = command
 		self.command_lines = command.split('\\\n')
+
+		if len(self.command_lines) != 27:
+			print("Error: command should have 27 lines")
+			sys.exit(1)
+
+		print("Successfully read command.")
+
 		self.command_lines[0] = "curl -o /dev/null -s -w \"%{http_code}\" 'https://www.instagram.com/graphql/query'"
 		self.data_raw = self.command_lines[-1]
 
@@ -33,26 +42,83 @@ class CommentCurlRequest:
 			flags=re.DOTALL
 		)
 
-		self._send_request()
+		return self._send_request()
 
 	def _send_request(self):
 		command = ' '.join(self.command_lines)
 		result = subprocess.run(command, shell=True, capture_output=True, text=True)
-		print(result.stdout)
+		return result.stdout
 
+
+# class RandomEmojis():
+# 	emojis = ["🔥", "🙏", "❤️", "🙌", "🫶", "🏔️"]
+
+# 	def __init__(self):
+# 		self.current_emoji = random.randint(0, len(RandomEmojis.emojis))
+	
+# 	def generate_comment(self):
+# 		val = self.current_emoji * random.randint(1, 7)
+# 		if random.random() < 0.2:
+# 			self.current_emoji = random.randint(0, len(RandomEmojis.emojis))
+# 		return val
+
+
+SWITCH_COMMENT_PROBABILITY = 0.15
+MIN_REQUEST_TIME_THRESHOLD_MS = 500
+LOOP_ITERATIONS = 5000
+MAX_CONSECUTIVE_WARNINGS = 10
 
 
 def main():
 	
-	print("Copy your cURL command from chrome and hit enter: ")
+	print("Copy your cURL command from Chrome, then come back and hit enter: ")
 	_ = input()
 	
 	# get input
 	command = pyperclip.paste()
 	curl_request = CommentCurlRequest(command)
-	curl_request.comment("SCO 1")
 
+	# get random comments
+	print("\nEnter a bunch of random comments to avoid making it look like you're a bot.")
+	print("Make it look realistic by using emojis and random letters that you would type if you were on your phone spamming comments")
+	print("The bot will repeat the comments a random amount of times and then switch to a different comment.")
+	print("\nWhen you're done, just press enter again\n")
 
+	comments = []
+	c = input("Comment  0: ")
+	while c != "":
+		comments.append(c)
+		c = input(f"Comment {len(comments): 2}: ")
+
+	# send comments
+	consecutive_warnings = 0
+	comment_idx = random.randint(0, len(comments) - 1)
+
+	for i in range(LOOP_ITERATIONS):
+		comment = comments[comment_idx]
+		print(f"{i + 1: 4} Commenting \"{comment}\" ... ", end="", flush=True)
+
+		start = time.perf_counter() * 1000
+		result = curl_request.comment(comment)
+		end = time.perf_counter() * 1000
+
+		if (end - start) < MIN_REQUEST_TIME_THRESHOLD_MS: 
+			print("Warning: request time was too short. This might mean the comment didn't go through")
+			consecutive_warnings += 1
+		elif result == "200":
+			print("success")
+		else:
+			print(f"Error {result}")
+			break
+
+		if random.random() < SWITCH_COMMENT_PROBABILITY:
+			comment_idx = random.randint(0, len(comments) - 1)
+		
+		if consecutive_warnings > MAX_CONSECUTIVE_WARNINGS:
+			print("Too many short requests in a row, comments are probably not going through. Time to switch accounts and try again!")
+			break
+		
+		
 
 
 if __name__ == '__main__':
--- script.zsh
+++ script.zsh
@@ -1,10 +1,12 @@
 #!/bin/zsh
 
 python_check=$(which python3)
-if [[ $python_check =~ "not found" ]]; then
+pip_check=$(which pip3)
+if [[ $python_check =~ "not found" || $pip_check =~ "not found" ]]; then
 	echo "You need to install Python first."
 	echo "Mac download: https://www.python.org/ftp/python/3.13.7/python-3.13.7-macos11.pkg"
 	exit
 fi
 
 pip3 install pyperclip
+
